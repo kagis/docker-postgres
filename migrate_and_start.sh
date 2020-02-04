169a2d8e926d43535e2157c6128a6e5d071c5e6b
Vladislav Nezhutin <exe-dealer@yandex.ru>
2020-02-04 09:50:38 +0300

    cosmetic

--- migrate_and_start.sh
+++ migrate_and_start.sh
@@ -8,8 +8,8 @@ pg_ctl -w start --timeout 3600 -o "$PGOPTS -c log_statement=all"
 find migrations -type d -mindepth 1 -maxdepth 1 -exec basename {} ';' \
 | xargs -I %DB% psql -d %DB% --no-align --tuples-only --field-separator ' ' -P null=. \
   -c "SELECT current_database(), current_setting('migration.latest', true)" \
-| xargs -rn2 sh -c 'ls -1 "migrations/$0" | awk -v DB="$0" -v M="$1" "/\.sql\$/ && \$0 > M { print DB, \$0 }"' \
-| xargs -rn2 sh -c $' \
+| xargs -rn2 sh -c 'ls -1 "migrations/$0" | awk -v DB="$0" -v M="$1" "/\.sql$/ && \$0 > M { print DB, \$0 }"' \
+| xargs -rn2 sh -c $'
   psql -d "$0" -v ON_ERROR_STOP=1 --single-transaction \
     -f "migrations/$0/$1" \
     -c "ALTER DATABASE \"$0\" SET migration.latest = \'$1\'" '
